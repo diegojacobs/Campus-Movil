74cf9dfca552d6ccc1d01f94acbe813fc1c98742
diegojacobs <diegojacobs@users.noreply.github.com>
2015-11-16 11:46:32 -0600

    Map

--- app/src/main/java/com/example/usuario/uvgmovil/MapsActivity.java
+++ app/src/main/java/com/example/usuario/uvgmovil/MapsActivity.java
@@ -4,6 +4,7 @@ import android.app.FragmentTransaction;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toolbar;
 
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -26,7 +27,16 @@ public class MapsActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        setUpMapIfNeeded(14.604143, -90.492913, "Parqueo 1");
+
+        mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
+                .getMap();
+
+        mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+        setUpMapIfNeeded(14.604049, -90.492778, "Parqueo 1",BitmapDescriptorFactory.HUE_RED);
+        setUpMapIfNeeded(14.604976, -90.488008, "Puerta 8",BitmapDescriptorFactory.HUE_GREEN);
+        setUpMapIfNeeded(14.605321, -90.487757, "Parqueo 9",BitmapDescriptorFactory.HUE_RED);
+        CameraUpdate mCamera = CameraUpdateFactory.newLatLngZoom(new LatLng(14.606461, -90.490743), 16);
+        mMap.animateCamera(mCamera);
     }
 
     @Override
@@ -37,7 +47,7 @@ public class MapsActivity extends FragmentActivity {
     /**
      * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
      * installed) and the map has not already been instantiated.. This will ensure that we only ever
-     * call {@link #setUpMap(double lng, double lat, String marker)} once when {@link #mMap} is not null.
+     * call {@link #setUpMap(double lng, double lat, String marker, float color)} once when {@link #mMap} is not null.
      * <p/>
      * If it isn't installed {@link SupportMapFragment} (and
      * {@link com.google.android.gms.maps.MapView MapView}) will show a prompt for the user to
@@ -49,17 +59,29 @@ public class MapsActivity extends FragmentActivity {
      * stopped or paused), {@link #onCreate(Bundle)} may not be called again so we should call this
      * method in {@link #onResume()} to guarantee that it will be called.
      */
-    private void setUpMapIfNeeded(double lat, double lng, String marker) {
+    private void setUpMapIfNeeded(double lat, double lng, String marker, float color) {
         // Do a null check to confirm that we have not already instantiated the map.
         if (mMap == null) {
             // Try to obtain the map from the SupportMapFragment.
             mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
                     .getMap();
+
             // Check if we were successful in obtaining the map.
             if (mMap != null) {
                 mMap.setMyLocationEnabled(true);
-                setUpMap(lat, lng, marker);
+                setUpMap(lat, lng, marker,color);
+                System.out.println(lat);
+                System.out.println(lng);
+                System.out.println(marker);
             }
+        }
+        // Check if we were successful in obtaining the map.
+        if (mMap != null) {
+            mMap.setMyLocationEnabled(true);
+            setUpMap(lat, lng, marker,color);
+            System.out.println(lat);
+            System.out.println(lng);
+            System.out.println(marker);
         }
     }
 
@@ -69,9 +91,11 @@ public class MapsActivity extends FragmentActivity {
      * <p/>
      * This should only be called once and when we are sure that {@link #mMap} is not null.
      */
-    private void setUpMap(double lat, double lng, String marker) {
-        mMap.addMarker(new MarkerOptions().position(new LatLng(lat, lng)).title(marker));
-        CameraUpdate mCamera = CameraUpdateFactory.newLatLngZoom(new LatLng(lat, lng), 14);
-        mMap.animateCamera(mCamera);
+    private void setUpMap(double lat, double lng, String tittle, float color) {
+        MarkerOptions marker= new MarkerOptions()
+                .title(tittle)
+                .position(new LatLng(lat,lng))
+                .icon(BitmapDescriptorFactory.defaultMarker(color));
+        mMap.addMarker(marker);
     }
 }
